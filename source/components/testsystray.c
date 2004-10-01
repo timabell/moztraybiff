c7af067c216cf8a2d270e19613202646fb9821e2
future <future>
2004-10-01 03:01:14 +0000

    Be flexible in testing; receive image file name as argument.

--- source/components/testsystray.c
+++ source/components/testsystray.c
@@ -1,11 +1,11 @@
 #include "eggstatusicon.h"
 #include <gtk/gtk.h>
-#include "mozilla_mailnews.xpm"
 
 int main(int argc, char *argv[])
 {
 	gtk_init(&argc, &argv);
-	GdkPixbuf* pixbuf = gdk_pixbuf_new_from_xpm_data(xpm_mozilla_mailnews_icon);
+	GdkPixbuf* pixbuf = gdk_pixbuf_new_from_file(argv[1], NULL);
+	g_assert(pixbuf);
 	// This should be ideally replaced by a completely libpr0n-based icon rendering.
 	EggStatusIcon* trayIcon = egg_status_icon_new_from_pixbuf(pixbuf);
 	g_object_unref(G_OBJECT(pixbuf));
